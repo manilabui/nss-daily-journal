d5ed49009111d94d82948816efa7825624965713
Manila Bui <manila.bui@gmail.com>
2019-11-04 23:16:37 -0600

    finished delete feature + fixed save functionality to prevent saving entries with invalid patterns

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -2,7 +2,7 @@ import createApp from './components/App.js';
 import createForm from './components/Form.js';
 import createFilter from './components/Filter.js';
 import createEntry from './components/Entry.js';
-import { recordEntry, filterEntries } from './handlers.js';
+import { handleDelete, handleSave, handleFilter } from './handlers.js';
 import { getEntries } from './api.js';
 
 const container = document.querySelector('.container');
@@ -11,14 +11,18 @@ const moods = ['Dope', 'Chill', 'Whelmed'];
 
 export const renderEntries = entries => {
 	const log = document.querySelector(".entries-log");
-
 	let entriesStr = '';
 
 	entries.forEach(entry => entriesStr += createEntry(entry));
-
-
-
 	log.innerHTML = entriesStr;
+
+	const deleteButtons = document.querySelectorAll('.button-delete');
+
+	deleteButtons.forEach(button => {
+		const id = button.id.split("--")[1];
+
+		button.addEventListener('click', () => handleDelete(id));
+	});
 };
 
 export const renderApp = () => {
@@ -34,6 +38,6 @@ export const renderApp = () => {
 	const saveEntryButton = document.querySelector('.button-save');
 	const moodFilterButtons = document.querySelectorAll('.moodFilter');
 
-	saveEntryButton.addEventListener('click', recordEntry);
-	moodFilterButtons.forEach(button => button.addEventListener('click', () =>filterEntries(button.value)));
+	saveEntryButton.addEventListener('click', handleSave);
+	moodFilterButtons.forEach(button => button.addEventListener('click', () => handleFilter(button.value)));
 };
--- src/scripts/handlers.js
+++ src/scripts/handlers.js
@@ -1,26 +1,36 @@
-import { getEntries, postEntry } from './api.js';
+import { getEntries, postEntry, deleteEntry } from './api.js';
 import { renderEntries } from './dom.js';
 
-const getValue = selector => document.querySelector(selector).value;
+const getValue = selector => {
+	const input = document.querySelector(selector);
 
+	return input.checkValidity() ? input.value : false;
+};
+// don't actually see the point of this right now
 const createEntryObj = (date, concept, entry, mood) => ({ date, concept, entry, mood });
 
-export const recordEntry = () => {
+export const handleSave = () => {
 	const date = getValue('.input-date');
 	const concepts = getValue('.input-concepts');
 	const entry = getValue('.input-entry');
 	const mood = getValue('.input-mood');
 
-	if (!date || !concepts || !entry) return console.log("Entry is incomplete.");
+	if (!date || !concepts || !entry) return console.log("Invalid entry.");
 
 	const newEntry = createEntryObj(date, concepts, entry, mood);
 	// any function that returns a promise, you can chain a .then to, which fetch calls return promises
 	postEntry(newEntry)
-		.then(getEntries())
+		.then(getEntries)
 		.then(entries => renderEntries(entries));
 };
 
-export const filterEntries = mood => {
+export const handleFilter = mood => {
 	getEntries()
 		.then(entries => renderEntries(entries.filter(entry => entry.mood === mood)));
+};
+
+export const handleDelete = id => {
+	deleteEntry(id)
+		.then(getEntries)
+		.then(entries => renderEntries(entries));
 };
